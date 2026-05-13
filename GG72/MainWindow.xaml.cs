9f53aa71854852bc7f742e1704c03eccfef11824
Valery <vsler@YCHEBA>
2026-05-13 22:59:26 +0300

    Окно приложения и все кнопки и возможности по ТЗ

--- GG72/MainWindow.xaml.cs
+++ GG72/MainWindow.xaml.cs
@@ -12,17 +12,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static GG72.Fynkcya;
 
 namespace GG72
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    /// </summar
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Кнопка "Зашифровать"
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string input = InputTextBox.Text;
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("Введите текст");
+
+            }
+            else
+            {
+                string encoded = Rot13(input);
+                OutputTextBox.Text = encoded;
+            }
+        }
+
+        // Кнопка "Расшифровать" (делает то же самое, так как ROT13 самодвойственен)
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("Введите текст");
+
+            }
+            else
+            {
+                string decoded = Rot13(input);
+                OutputTextBox.Text = decoded;
+            }
+        }
+
+
+        private void InputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
